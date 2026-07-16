3961d14c57f6176901c88ff0001cdf5549eeb4d6
Neo_1 <michaeloogwu58.com>
2026-07-16 16:39:05 +0100

    practice

---
+++ answers/exercise6/answer.py
@@ -0,0 +1,52 @@
+age = 18 
+
+if age >= 21:
+    print("You can enter the club and can buy drinks.")
+elif age >= 18:
+    print("You can enter the club but cannot buy drinks.")
+else:
+    print("You are too young to enter the club")
+
+    has_id  = True
+    is_vip = False
+
+name = input("Enter your name: ")
+if name:
+    print(f"Hello, {name}!")
+else:
+    print("You didn't enter a name.")
+
+x = 10 
+status = "even" if x % 2 == 0 else "odd"
+print(status)
+
+command = "start"
+
+match command:
+    case "start":
+        print("Starting the system...")
+    case "stop" | "halt":
+        print("Stopping the system...")
+    case _:
+        print("Unknown command.")
+
+
+mark = 92
+if mark >= 90 and mark <= 100:
+    print("A")
+elif mark < 90 and mark >= 80:
+    print("B")
+elif mark < 80 and mark >= 70:
+    print("C")
+else: 
+    print ("F")
+
+score = 85
+if score >= 90 and score <= 100 :
+    print(f"score is {score}, EXCELLENT!" )
+elif score < 90 and score >= 70 :
+    print(f"score is {score}, GOOD!")
+elif score < 70 and score >= 0:
+    print(f"score is {score}, NEEDS IMPROVEMENT!")
+else:
+    print("invalid score, score must be between 0 to 100")
--- python-study-Neo_1/curriculum/06_control_flow/exercises.py
+++ python-study-Neo_1/curriculum/06_control_flow/exercises.py
@@ -6,15 +6,7 @@
 # - "Excellent" if score is 90 or above.
 # - "Good" if score is 70 to 89.
 # - "Needs Improvement" if score is below 70.
-score = 85
-if score >= 90 and score <= 100 :
-    print(f"score is {score}, EXCELLENT!" )
-elif score < 90 and score >= 70 :
-    print(f"score is {score}, GOOD!")
-elif score < 70 and score >= 0:
-    print(f"score is {score}, NEEDS IMPROVEMENT!")
-else:
-    print("invalid score, score must be between 0 to 100")
+
 
 
 # TODO: Exercise 2
@@ -22,8 +14,6 @@ else:
 # if the age variable is 18 or older, and "Cannot Vote" otherwise.
 age = 20
 
-can_vote = "can vote" if age > 17 else "cannot vote"
-print(can_vote)
 
 
 # TODO: Exercise 3
@@ -32,24 +22,9 @@ print(can_vote)
 # - If role is "editor" or "author", print "Write access"
 # - If role is "guest", print "Read access"
 # - Otherwise, print "No access"
-role = input("Are you an admin, editor, author, or guest? \n Enter your role: ")
-match role:
-    case "admin":
-        print("full access")
-    case "editor" | "author":
-        print("write access")
-    case "guest":
-        print("read access")
-    case _ :
-        print("no access")
+
 
 
 # TODO: Exercise 4
 # Check if the list 'items' is empty or not using truthy/falsy check.
 # Print "Empty list" or "Items present".
-items = []
-
-if items:
-    print("Items present")
-else:
-    print("Empty list")
