624fcb57f52a3b8346fe325451387e318ca7401f
dab00gieman <michaeloogwu58.com>
2026-07-17 13:39:15 +0100

    practice

--- answers/exercise7/answer.py
+++ answers/exercise7/answer.py
@@ -16,3 +16,27 @@ count = 1
 while count <= 5:
     print(count)
     count += 1
+
+for num in range(10):
+    if num == 5:
+        break
+    print(num)  
+
+
+for num in range(5):
+    if num == 2:
+        continue
+    print(num)
+
+for num in range(5):
+    print(num)
+else:
+    print("Loop finished successfully!")
+
+
+squares = [x ** 2 for x in range(10) if x % 2 == 0]
+print(squares)
+
+
+squares_dict = {x: x ** 2 for x in range(5)}
+print(squares_dict)
