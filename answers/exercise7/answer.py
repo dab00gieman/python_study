e81d25c7bace8e0bd653b05dd242f7f56e72c935
Neo_1 <michaeloogwu58.com>
2026-07-17 10:27:33 +0100

    practice

--- answers/exercise7/answer.py
+++ answers/exercise7/answer.py
@@ -1,3 +1,18 @@
 fruits = ["apple", "banana", "cherry"]
 for fruit in fruits:
     print(fruit)
+
+for m in range(5):
+    print(m)
+
+for m in range(2, 6):
+    print(m)
+
+for m in range(0, 10, 2):
+    print(m)
+
+
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
